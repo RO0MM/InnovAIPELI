1d6b52473813599bdd29b35af339347096fffa17
RO0MM <sotorommel22@gmail.com>
2026-05-12 17:34:40 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,5 +8,5 @@ window.INNOV_CONFIG = {
   APP_NAME: "INNOV IA PLAY",
   SUPABASE_URL: "https://upqbncbobeoqzrwlfnsw.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_KlpqVx6Ovg3q9QXx9Ctjnw_TqSr_o_W",
-  EDGE_PLAYBACK_URL: "EDGE_PLAYBACK_URL: "https://upqbncbobeoqzrwlfnsw.supabase.co/functions/v1/playback-url""
+  EDGE_PLAYBACK_URL: "EDGE_PLAYBACK_URL: "https:/upqbncbobeoqzrwlfnsw.supabase.co/functions/v1/playback-url"
 };
